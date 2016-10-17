8e5e350a02592a3166c33d2311d48e7b20840e6b
infamousflame <Owenlevin@gmail.com>
2016-10-17 19:23:16 +0000

    init commit

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+var header = document.getElementById("header");
+header.innerHTML = "<h1>Random Tip Generator</h1> <p>This is a description of the project</p>"
+header.style = "background-color:grey";
