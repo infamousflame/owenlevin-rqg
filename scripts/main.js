99798a92a74b878c95330bb13f1d899985a2d360
infamousflame <Owenlevin@gmail.com>
2016-10-17 19:26:08 +0000

    added alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 var header = document.getElementById("header");
 header.innerHTML = "<h1>Random Tip Generator</h1> <p>This is a description of the project</p>"
 header.style = "background-color:grey";
+alert("easy");
